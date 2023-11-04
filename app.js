e57246e501ea733b61bd51580b22c09195feb7fc
Ludmila Muñoz <ludmi.m.1109@gmail.com>
2023-11-03 22:58:07 -0300

    Agrego app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const PORT = 3030;
+const path = require('path');
+
+app.use(express.static(path.join(__dirname,'public')));
+
+app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views','home.html')));
+
+app.listen(PORT,() => console.log(`Servidor corriendo en http://localhost:${PORT}`));
