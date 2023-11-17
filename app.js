77ec107813844755d117aee55c9898d03c9e8b15
Ludmila Muñoz <ludmi.m.1109@gmail.com>
2023-11-17 00:10:02 -0300

    agrego estructura a app.js y footer.html

--- app.js
+++ app.js
@@ -11,4 +11,7 @@ app.get('/register', (req,res) => res.sendFile(path.join(__dirname, 'views','reg
 app.get('/productCart', (req,res) => res.sendFile(path.join(__dirname, 'views','productCart.html')));
 app.get('/productDetail', (req,res) => res.sendFile(path.join(__dirname, 'views','productDetail.html')));
 
+app.get('/footer', (req,res) => res.sendFile(path.join(__dirname, 'views','partials','footer.html')));
+app.get('/header', (req,res) => res.sendFile(path.join(__dirname, 'views','partials','header.html')));
+
 app.listen(PORT,() => console.log(`Servidor corriendo en http://localhost:${PORT}`));
