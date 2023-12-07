c56c3fd97fba04c3187eddd2c061962c4d16859e
Rony Mari <rony.s.mari@hotmail.com>
2023-12-06 22:24:42 -0300

    rony

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,11 +1,32 @@
+// productsController.js
+const productsData = require('../data/index');
+
 module.exports = {
-    detail: (req,res) => {
-        res.render('products/productDetail');
-    },
-    detailPost: (req,res) => {
+    detailPost: (req, res) => {
+        // Implementa la lógica para /detallePublicacion (si es necesario)
         res.render('products/productDetailPost');
     },
-    edit : (req,res) => {
-        return res.render('products/product-edit')
+    detail: (req, res) => {
+        // Implementa la lógica para /detalle (si es necesario)
+        res.render('products/productDetail');
+    },
+    edit: (req, res) => {
+        // Obtén el ID del producto desde los parámetros de la solicitud
+        const productId = req.query.id;
+
+        // Si se proporciona productId, obtén los detalles del producto; de lo contrario, crea un nuevo objeto de producto
+        const product = productId ? productsData.servicios.find(p => p.id == productId) : {
+            id: '',
+            name: '',
+            description: '',
+            skills: '',
+            portfolio: '',
+            redes_sociales: '',
+            images: [],
+            category: '',
+            price: ''
+        };
+
+        res.render('products/product-edit', { product });
     }
-}
+};
