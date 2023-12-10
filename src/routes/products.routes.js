4543575b11d959cb1e815f969822ae2502ccea32
Ludmila Muñoz <ludmi.m.1109@gmail.com>
2023-12-09 23:35:47 -0300

    Completo estructura de productDetailPost

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -6,6 +6,7 @@ const router = express.Router();
 router
   .get('/detallePublicacion', detailPost)
   .get('/detalle', detail)
+  /*.get('/editar', edit)*/
   .get('/editar/:id?', edit);
 
 module.exports = router;
