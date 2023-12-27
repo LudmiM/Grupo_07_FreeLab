2208359831d162dab843024630b88e51a07b023d
Antonio <marco.a.falcon@outlook.com>
2023-12-27 13:37:53 -0300

    multer agregado + logica para subir imagenes

---
+++ src/middleware/upload.js
@@ -0,0 +1,18 @@
+const multer = require('multer');
+const path = require('path')
+
+const storage =  multer.diskStorage({
+    destination : (req, file, cb) =>{
+        cb(null, path.join(__dirname, '../../public/images/productos/'))
+    },
+
+    filename : (req, file, cb) => {
+        cb(null, `${Date.now()}_products_${file.originalname}${path.extname(file.originalname)}`)
+    } 
+}) 
+
+const upload = multer({
+    storage
+})
+
+module.exports = upload
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const { detailPost, detail, addPost, edit, formProduct } = require('../controllers/productsController');
+const upload = require('../middleware/upload');
 const router = express.Router();
 
 /* GET users /productos */
 router
   .get('/detallePublicacion',detailPost)
   .get('/formularioProducto',formProduct)
-  .post('/agregar',addPost)
+  .post('/agregar', upload.single('image'), addPost)
   .get('/detalle',detail)
   .get('/editar/:id?', edit);
 
