ac061e08251d0fbc51ae32ff3fa3536242af0e78
Ludmila Muñoz <ludmi.m.1109@gmail.com>
2024-01-13 21:38:23 -0300

    Separo archivo productsController en careta productsController + hojas .js necesarias

---
+++ src/controllers/productsController/eliminate.js
@@ -0,0 +1,34 @@
+const data = require('./../../data');
+const fs = require('fs');
+const { existsSync, unlinkSync, renameSync } = require('fs');
+const path = require('path');
+
+
+module.exports = (req,res) => {
+    const products = data.leerJSON('products');
+    const productId = parseInt(req.params.id); // Convierte el id a número
+  
+    const productToDelete = products.servicios.find(p => p.id === productId);
+  
+    if (!productToDelete) {
+      return res.status(404).send('Producto no encontrado');
+    }
+  
+    const { image } = productToDelete || {};
+  
+    // Eliminar todas las imágenes asociadas al producto
+    if (image && image.length > 0) {
+      image.forEach(img => {
+        const imagePath = path.join(__dirname, '../../public/images/productos/', img);
+        existsSync(imagePath) && unlinkSync(imagePath);
+      });
+    }
+  
+    // Resto del código para eliminar el producto
+    const sinEliminado = products.servicios.filter(p => p.id !== productId);
+  
+    const updatedProducts = { ...products, servicios: sinEliminado };
+    data.escribirJSON(updatedProducts, 'products');
+  
+    res.redirect('/admin');
+  }
---
+++ src/controllers/productsController/index.js
@@ -0,0 +1,19 @@
+const eliminate = require('./eliminate')
+const formProduct = require('./formProduct')
+const updatedProduct = require('./updateProduct')
+const edit = require('./edit')
+const detailPost = require('./detailPost')
+const detail = require('./detail')
+const addPost = require('./addPost')
+const listadoProducts = require('./listadoProducts')
+
+module.exports = {
+    eliminate,
+    addPost,
+    detail,
+    detailPost,
+    edit,
+    updatedProduct,
+    formProduct,
+    listadoProducts
+}
