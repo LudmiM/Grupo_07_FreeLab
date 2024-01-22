259e44bd51a784886a4192f1d5db4d2217be73d8
Ludmila Muñoz <ludmi.m.1109@gmail.com>
2024-01-22 01:43:27 -0300

    Solucion a problema de construccion de rutas

--- src/controllers/usersController/logout.js
+++ src/controllers/usersController/logout.js
@@ -2,10 +2,7 @@ const data = require('./../../data');
 
 module.exports = (req,res) => {
         
-    req.session.destroy();/*
-    res.cookie('kitchening4EV3R_user',null,{
-        maxAge : -1
-    })*/
+    req.session.destroy();
 
     return res.redirect('/')
 }
