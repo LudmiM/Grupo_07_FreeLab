704a5b69b5e263a48320453f50050d47ff7a9cd8
Ludmila Muñoz <ludmi.m.1109@gmail.com>
2024-02-04 19:30:38 -0300

    intento adaptar cambios

--- src/validations/perfil-edit-personal.js
+++ src/validations/perfil-edit-personal.js
@@ -2,7 +2,7 @@ const { check } = require("express-validator");
 
 const editPersonalValidator = [
     check('freelancerFirstname')
-        .notEmpty().withMessage('El nombre no debe estar vacio')/*,
+        .notEmpty().withMessage('El nombre no debe estar vacio'),
     check('freelancerLastname')
         .notEmpty().withMessage('El apellido no debe estar vacio'),
     check('freelancerPhoneCode')
