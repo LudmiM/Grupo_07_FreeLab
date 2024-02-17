3a3475cf15b347bd0e6ced8d24ebb9f74b38f1f7
Ludmila Muñoz <ludmi.m.1109@gmail.com>
2024-02-16 23:49:02 -0300

    instalo variables de entorno + dotenv y .env y aplicacion

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,9 +1,12 @@
+require('dotenv').config();
+
 module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USERNAME_DEV,
+    "password": process.env.DB_PASSWORD_DEV,
+    "database": process.env.DB_DATABASE_DEV,
+    "host": process.env.DB_HOST_DEV,
+    "port": process.env.DB_PORT_DEV,
     "dialect": "mysql"
   },
   "test": {
