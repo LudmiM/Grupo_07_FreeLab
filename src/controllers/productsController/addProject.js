47cb153564cb4bbeec12ffd072fe36584bef8901
Milagros <miilyailen97@gmail.com>
2024-02-29 02:34:12 -0300

    agrego la refactorización de creacion de productos

--- src/controllers/productsController/addProject.js
+++ src/controllers/productsController/addProject.js
@@ -1,28 +1,49 @@
-const data = require('./../../data/index');
+const db = require("../../database/models");
 
 module.exports = (req, res) => {
-    const { title,description,price,category, idUser } = req.body;
+   // if(error.isEmpty()) { 
+  const { title,description,idStatus,idCompany,price,category} = req.body;
   
-    const publications = data.leerJSON('publications');
+  
+  db.Project.create({
+    title,
+    description,
+    idStatus,
+    idCompany,
+    price,
+    category,
+    createAt : new Date,
+    updateAt : new Date
+  })
+  .then(project => {
+    console.log(project);
+    return res.redirect('/usuarios/perfil');
+  })
+  .catch(error => console.log(error))
+}
+
+ //}
+
+   // const publications = data.leerJSON('publications');
     
-    const lastId = publications.length > 0 ? parseInt(publications[publications.length - 1].id) : 0;
+    // const lastId = publications.length > 0 ? parseInt(publications[publications.length - 1].id) : 0;
 
     // Crear un nuevo id consecutivo
-    const newId = lastId + 1;
+   // const newId = lastId + 1;
   
-    function project(title, description,category, price) {
-      this.id = newId; // No convertir a cadena para que sea un número
-      this.title = title;
-      this.description = description;
-      this.category = category;
-      this.price = price;
-      this.idUser = idUser;
-    }
+    //function project(title, description,category, price) {
+   //   this.id = newId; // No convertir a cadena para que sea un número
+   //   this.title = title;
+   //   this.description = description;
+   //   this.category = category;
+   //   this.price = price;
+   //   this.idUser = idUser;
+   // }
   
-    const newproject = new project(title, description, category, price,idUser);
+   /* const newproject = new project(title, description, category, price,idUser);
 
     publications.push(newproject);
     data.escribirJSON(publications, 'publications');
   
     res.redirect('/usuarios/perfil');
-  }
+  } */
--- src/database/models/Project.js
+++ src/database/models/Project.js
@@ -13,17 +13,26 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  Project.init({
-    title: DataTypes.STRING,
-    description: DataTypes.TEXT,
-    createdAt: DataTypes.DATE,
-    updatedAt: DataTypes.DATE,
-    idStatus: DataTypes.INTEGER,
-    idCompany: DataTypes.INTEGER,
-    price: DataTypes.INTEGER
-  }, {
+  Project.init(
+    {
+    title: { type : DataTypes.STRING,
+    allownull : false },
+    description: { type : DataTypes.TEXT,
+      allowNull : true },
+    createdAt: {type : DataTypes.DATE,
+      allowNull : false  },
+    updatedAt:{ type : DataTypes.DATE,
+      allownull : false  },
+    idStatus: { type : DataTypes.INTEGER,
+      allowNull : true  },
+    idCompany: { type : DataTypes.INTEGER,
+      allowNull : true },
+    price: { type : DataTypes.INTEGER,
+      allowNull : true },
+     }, {
     sequelize,
     modelName: 'Project',
-  });
+  }
+  );
   return Project;
 };
