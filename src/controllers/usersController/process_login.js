c6783518673321f375b59434f0958d1bb771a552
Ludmila Muñoz <ludmi.m.1109@gmail.com>
2024-03-02 19:40:47 -0300

    Agrego async y await a process_login

--- src/controllers/usersController/process_login.js
+++ src/controllers/usersController/process_login.js
@@ -1,35 +1,39 @@
 const { validationResult } = require("express-validator");
 const db = require('./../../database/models')
 
-module.exports = (req, res) => {
-    const errors = validationResult(req);
-    const { email, remember } = req.body;
+module.exports = async (req, res) => {
+    try {
+        const errors = validationResult(req);
 
-    if (errors.isEmpty()) {
-        db.User.findOne({
+        if (!errors.isEmpty()) {
+            return res.render('users/login', {
+                errors: errors.mapped()
+            });
+        }
+        const emailOfBody = req.body.email.trim().toLowerCase();
+
+        const user = await db.User.findOne({
             where: {
-                email:email
+                email: emailOfBody
             }
-        }).then((user) => {
-            const {idRole, id, email}=user.dataValues
-                req.session.userLogin = {
-                    idRole, id, email
-                }
-                console.log('Muestrameee')
-                console.log(req.session.userLogin)
-
-                if (remember) {
-                    res.cookie('FreeLab_user_Login_01', req.session.userLogin, {
-                        maxAge: 1000 * 60 * 5 
-                    });
-                }
-
-                return idRole != 3 ? res.redirect('/') : res.redirect('/admin')
-        }).catch(error => console.log(error),
-        )
-    } else {
-        return res.render('users/login', {
-            errors: errors.mapped()
         });
+
+        const { idRole, id, email } = user.dataValues
+        req.session.userLogin = {
+            idRole, id, email
+        }
+        console.log('Muestrameee')
+        console.log(req.session.userLogin)
+
+        if (req.body.remember) {
+            res.cookie('FreeLab_user_Login_01', req.session.userLogin, {
+                maxAge: 1000 * 60 * 5
+            });
+        }
+
+        return idRole != 3 ? res.redirect('/') : res.redirect('/admin')
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send('Error interno del servidor');
     }
 };
