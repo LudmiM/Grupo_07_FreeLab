9d4a8d1d85570fcf47739cee9998af1a9a876b47
Ludmila Muñoz <ludmi.m.1109@gmail.com>
2024-03-04 18:12:13 -0300

    ...

--- src/controllers/productsController/eliminate.js
+++ src/controllers/productsController/eliminate.js
@@ -1,10 +1,10 @@
 //CAMBIAR ESTO, ACTUALMENTE ELIMINA LOS DATOS SECUNDARIOS DEL USUARIO;
 //MODIFICARLO PARA QUE ELIMINE U N POST/PUBLICACION  DE EMPLEO
-
+/*
 const data = require('./../../data');
 const fs = require('fs');
 const { existsSync, unlinkSync, renameSync } = require('fs');
-const path = require('path');
+const path = require('path');*/
 const db = require('../../database/models')
 
 module.exports = (req,res) => {
