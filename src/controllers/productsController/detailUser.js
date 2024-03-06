e2eb09c054efdd3fcf6e2bd08dd4fdc1193707f0
Ludmila Muñoz <ludmi.m.1109@gmail.com>
2024-03-05 21:58:38 -0300

    Renombro y refactorizo archivos, para que consuman la BBDD

--- src/controllers/productsController/detailUser.js
+++ src/controllers/productsController/detailUser.js
@@ -3,9 +3,9 @@ const db = require('./../../database/models');
 module.exports = async (req,res) => {
   try {
     const id = +req.params.id;
-    const p = await db.Project.findByPk(id);
-    
-    return res.render('products/productDetail', { usuario });
+    const u = await db.Project.findByPk(id);
+    console.log(req.url)
+    return res.render('products/user-detail', { u });
   } catch (error) {
     console.error(error);
     return res.status(500).send('Error interno del servidor');
