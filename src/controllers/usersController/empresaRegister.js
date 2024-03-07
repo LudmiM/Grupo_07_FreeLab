f6a102f141681268fc37d03488063d2840df4694
Rony Mari <rony.s.mari@hotmail.com>
2024-03-07 18:33:16 -0300

    Register - cambio de campos a bd

--- src/controllers/usersController/empresaRegister.js
+++ src/controllers/usersController/empresaRegister.js
@@ -1,40 +1,44 @@
 const { validationResult } = require("express-validator");
-const data = require('../../data'); 
+const data = require('../../data');
 const { Company, User } = require('../../database/models');
 const bcryptjs = require('bcryptjs');
 
-module.exports = (req, res) => {
-  const errors = validationResult(req);
-  const { companyName, userEmail, userPassword, employerPhoneCode, employerPhone, companyDescription, confirmPassword } = req.body;
-  const users = data.leerJSON('usuarios');
-  const file = req.file;
+module.exports = async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.render('users/empresaForm', {
+        old: req.body,
+        errors: errors.mapped()
+      });
+    }
 
-  const lastId = users.empresas.length > 0 ? parseInt(users.empresas[users.empresas.length - 1].id) : 0;
-  const newId = lastId + 1;
-  const empresaRole = 1; // ID del rol de empresa
+    const { companyName, location, companyDescription } = req.body;
+    const file = req.file;
 
-  function Empresa(companyName, userEmail, userPassword, employerPhoneCode, employerPhone, mainImage, companyDescription) {
-    this.id = newId;
-    this.companyName = companyName;
-    this.userEmail = userEmail;
-    this.userPassword = bcryptjs.hashSync(userPassword, 10);
-    this.employerPhoneCode = employerPhoneCode;
-    this.employerPhone = employerPhone;
-    this.mainImage = mainImage;
-    this.companyDescription = companyDescription;
-    this.idRole = empresaRole; // Asignar el ID del rol de empresa
-  }
+    const users = data.leerJSON('usuarios');
 
-  if (errors.isEmpty()) {
-    const newEmpresa = new Empresa(companyName, userEmail, userPassword, employerPhoneCode, employerPhone, file.filename, companyDescription);
+    const lastId = users.empresas.length > 0 ? parseInt(users.empresas[users.empresas.length - 1].id) : 0;
+    const newId = lastId + 1;
+    const empresaRole = 1; // ID del rol de empresa
+
+    function Empresa(companyName, location, mainImage, companyDescription) {
+      this.id = newId;
+      this.companyName = companyName;
+      this.location = location;
+      this.mainImage = mainImage;
+      this.companyDescription = companyDescription;
+      this.idRole = empresaRole; // Asignar el ID del rol de empresa
+    }
+
+    const newEmpresa = new Empresa(companyName, location, file.filename, companyDescription);
 
     users.empresas.push(newEmpresa);
     data.escribirJSON(users, 'usuarios');
     return res.redirect('/usuarios/ingreso');
-  } else {
-    return res.render('users/empresaForm', {
-      old: req.body,
-      errors: errors.mapped()
-    });
+  } catch (error) {
+    // Maneja los errores aquí
+    console.error(error);
+    return res.status(500).send("Error interno del servidor");
   }
 };
--- src/controllers/usersController/freelancerRegister.js
+++ src/controllers/usersController/freelancerRegister.js
@@ -3,39 +3,47 @@ const data = require('../../data');
 const { Freelancer, User } = require('../../database/models');
 const bcryptjs = require('bcryptjs');
 
-module.exports = (req, res) => {
-  const errors = validationResult(req);
-  const { freelancerFirstname, freelancerLastname, userEmail, userPassword, freelancerPhoneCode, freelancerPhone, freelancerSkills, confirmPassword } = req.body;
-  const users = data.leerJSON('usuarios');
-  const file = req.file;
+module.exports = async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.render('users/freelancerForm', {
+        old: req.body,
+        errors: errors.mapped()
+      });
+    }
 
-  const lastId = users.freelancers.length > 0 ? parseInt(users.freelancers[users.freelancers.length - 1].id) : 0;
-  const newId = lastId + 1;
-  const freelancerRole = 2; // ID del rol de freelancer
+    const { firstName, lastName, country, phoneCode, phone, about, hourValue } = req.body;
+    const file = req.file; // Suponiendo que estás usando multer o similar para subir archivos
 
-  function Freelancer(freelancerFirstname, freelancerLastname, userEmail, userPassword, freelancerPhoneCode, freelancerPhone, mainImage, freelancerSkills) {
-    this.id = newId;
-    this.freelancerFirstname = freelancerFirstname;
-    this.freelancerLastname = freelancerLastname;
-    this.userEmail = userEmail;
-    this.userPassword = bcryptjs.hashSync(userPassword, 10);
-    this.freelancerPhoneCode = freelancerPhoneCode;
-    this.freelancerPhone = freelancerPhone;
-    this.mainImage = mainImage;
-    this.freelancerSkills = freelancerSkills;
-    this.idRole = freelancerRole; // Asignar el ID del rol de freelancer
-  }
+    const users = data.leerJSON('usuarios');
 
-  if (errors.isEmpty()) {
-    const newFreelancer = new Freelancer(freelancerFirstname, freelancerLastname, userEmail, userPassword, freelancerPhoneCode, freelancerPhone, file.filename, freelancerSkills);
+    const lastId = users.freelancers.length > 0 ? parseInt(users.freelancers[users.freelancers.length - 1].id) : 0;
+    const newId = lastId + 1;
+    const freelancerRole = 2; // ID del rol de freelancer
+
+    function Freelancer(freelancerFirstname, freelancerLastname, userEmail, userPassword, freelancerPhoneCode, freelancerPhone, mainImage, freelancerSkills) {
+      this.id = newId;
+      this.freelancerFirstname = freelancerFirstname;
+      this.freelancerLastname = freelancerLastname;
+      this.userEmail = userEmail;
+      this.userPassword = bcryptjs.hashSync(userPassword, 10);
+      this.freelancerPhoneCode = freelancerPhoneCode;
+      this.freelancerPhone = freelancerPhone;
+      this.mainImage = mainImage;
+      this.freelancerSkills = freelancerSkills;
+      this.idRole = freelancerRole; // Asignar el ID del rol de freelancer
+    }
+
+    const newFreelancer = new Freelancer(firstName, lastName, country, phoneCode, phone, file.filename, about, hourValue);
 
     users.freelancers.push(newFreelancer);
     data.escribirJSON(users, 'usuarios');
+
     return res.redirect('/usuarios/ingreso');
-  } else {
-    return res.render('users/freelancerForm', {
-      old: req.body,
-      errors: errors.mapped()
-    });
+  } catch (error) {
+    // Maneja los errores aquí
+    console.error(error);
+    return res.status(500).send("Error interno del servidor");
   }
 };
