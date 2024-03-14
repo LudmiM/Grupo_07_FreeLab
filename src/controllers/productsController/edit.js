3d3adfe4b9890c608ec25d8227e9d48ead8fc10b
Milagros <miilyailen97@gmail.com>
2024-03-14 20:29:12 -0300

    refactorizacion para actualizar

--- src/controllers/productsController/edit.js
+++ src/controllers/productsController/edit.js
@@ -4,16 +4,21 @@ module.exports = async (req, res) => {
     const id = +req.params.id;
 
       try {
-        const project = await db.Project.findByPk(id);
-        console.log('this is project '+project+' el id es '+id)
-      
-    //habilidades y categorias const categories = await db.Category.findAll({});
-      const categories = await db.Category.findAll({});
+        const project = await db.Project.findByPk(id, {
+          include : [
+          
+            {model: db.Skill,as:'skills'}
 
+          ]
+        });
+        console.log('this is project '+project+' el id es '+id)
+        
+        const skills = project.skills;
+    
       res.render('products/project-edit', { 
-       project,
-        skills,
-        categories
+       project: project,
+       skills: skills
+        
       })
 
     } catch (error) {console.log(error)
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const { detailProject, formProject, detailUser, eliminate, edit, updateProduct, addProject } = require('../controllers/productsController');
+const { detailProject, formProject, detailUser, eliminate, edit, updateProduct, addProject} = require('../controllers/productsController');
 const upload = require('../middleware/upload');
 const checkRol = require('../middleware/checkRol');
+const updateProject = require('../controllers/productsController/updateProject');
 const router = express.Router();
 
 // /productos
@@ -12,6 +13,7 @@ router
   .post('/agregarProyecto', addProject)
   .get('/detalle/:id',detailUser)
   .delete('/eliminar/:id', eliminate) //ruta que elimina el proyecto creado
-  .get('/editar/:id?',checkRol.logged, edit)
+  .get('/editar/:id',checkRol.logged, edit)
+  .put('/actualizar/:id', updateProject)
   .post('/editar/:id?', upload.array('image', 5), updateProduct) // Cambiado a upload.array para permitir varios archivos
 module.exports = router;
