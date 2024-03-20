d70af86d0ad23df5c38f9c74d5f9facf1e4a6b0a
Ludmila Muñoz <ludmi.m.1109@gmail.com>
2024-03-20 00:31:13 -0300

    Creacion de proyectos + idea general de individuals

--- src/controllers/productsController/addProject.js
+++ src/controllers/productsController/addProject.js
@@ -1,12 +1,50 @@
 const db = require("../../database/models");
 
-module.exports = (req, res) => {
-   // if(error.isEmpty()) { 
-  const { titlePrincipal,descriptionPrincipal,idStatusPrincipal,skillesPrincipal} = req.body;
-  const idCompany = req.session.idCompany;
-  const formData = req.body;
+function cargarIndividuals(specialty,about,price,idProject,idCategory) {
+   const individual = db.Individual.create({
+      specialty: specialty,
+      about: about,
+      price: price,
+      idProject: idProject,
+      idCategory: idCategory,
+      chosen: false
+      })
+}
+function estructuraSkilles(skilles) {
+   const newSkills=skilles.map(s => {
+      return {
+         idProject: project.id,
+         idSkill: parseInt(s)
+      };
+   }) 
+   return newSkills
+}
 
-  console.log("Datos enviados desde el formulario:", formData);
-    
-  res.send('Datos recibidos correctamente', formData);
- }
+module.exports = (req, res) => {
+
+  try {
+   const { title,description,idStatus,skilles} = req.body;
+   const idCompany = req.session.idCompany;
+   
+   const project = db.Project.create({
+      title:title,
+      description: description,
+      idStatus: idStatus,
+      idCompany,
+      createdAt : new Date(),
+      updatedAt : new Date()
+   })
+   const newSkills=estructuraSkilles(skilles)
+   db.ProjectSkill.bulkCreate(newSkills)
+
+   const {specialty,about,price,idProject,idCategory}=req.body
+   cargarIndividuals(specialty,about,price,idProject,idCategory)
+   req.body.forEach(e => {
+      
+   });
+
+   return res.redirect('/usuarios/perfil');
+   } catch (error) {
+   
+   }
+}    
