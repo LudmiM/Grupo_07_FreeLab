fd7381dfadf236e13e22ea6b358a12a09f9e5481
Ludmila Muñoz <ludmi.m.1109@gmail.com>
2024-03-19 21:28:12 -0300

    Project-create formulario + js

--- src/controllers/productsController/addProject.js
+++ src/controllers/productsController/addProject.js
@@ -2,11 +2,11 @@ const db = require("../../database/models");
 
 module.exports = (req, res) => {
    // if(error.isEmpty()) { 
-  const { title,description,idStatus,category,skilles} = req.body;
+  const { titlePrincipal,descriptionPrincipal,idStatusPrincipal,skillesPrincipal} = req.body;
   const idCompany = req.session.idCompany;
   const formData = req.body;
 
-    console.log("Datos enviados desde el formulario:", formData);
+  console.log("Datos enviados desde el formulario:", formData);
     
-    res.send('Datos recibidos correctamente', formData);
+  res.send('Datos recibidos correctamente', formData);
  }
