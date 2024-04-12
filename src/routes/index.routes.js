99ad83bd2b7b9796bad04d02efde13758fc0155b
Ludmila Muñoz <ludmi.m.1109@gmail.com>
2024-04-11 22:05:41 -0300

    Soluciono problema con rutas

---
+++ src/routes/index.routes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const indexController = require('../controllers/indexController');
+const checkRol = require('./../middleware/checkRol');
+
+router
+  .get('/', indexController.index)
+  .get('/admin',checkRol.admin, indexController.admin)
+  .get('/buscar', indexController.search)
+  .post('/newsletter', indexController.newsletter)
+  .post('/guardar/:id')
+  .delete('/guardar/:id');
+  //Los freelancers pueden guardarproyectos, recibo el id del proyecto, pensar bien donde van en PRYECTRO o index router
+  //.get('/listar', indexController.listProjects);
+
+//router.get('/listado', indexController.listProducts);
+router.get('/carrito',checkRol.logged, (req, res) => {
+  res.render('productCart');
+});
+
+module.exports = router;
